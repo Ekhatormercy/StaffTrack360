f72f5ea5bc42529d4b63333e99dff709ee318503
Ekhatormercy <ekhatormercy07@gmail.com>
2024-02-25 17:59:03 +0100

    actives commit

--- src/pages/landingPage/Header/Headerlgn.jsx
+++ src/pages/landingPage/Header/Headerlgn.jsx
@@ -1,10 +1,17 @@
 import "./Headerlgn.css";
+import { useNavigate } from "react-router-dom";
 const Headerlgn = () => {
+
+  const Nav = useNavigate()
+
+  const handlehome =()=>{
+    Nav("/")
+  }
   return (
     <div className="Header">
       <div className="Headerwrap1">
         <div className="staflogodiv">
-          <h3 className="staflogodiv">
+          <h3 onClick={handlehome} className="staflogodiv">
             StaffTrack<span>360</span>
           </h3>
         </div>
