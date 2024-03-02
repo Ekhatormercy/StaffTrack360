67ac31c09ebcdb3977dd95898bb4ca5d852eeb98
ujahcollins <ujahcollins@gmail.com>
2024-03-02 17:36:05 +0100

    add employee Api consumption

--- src/pages/Dahboard/AdminDashboard/pages/AddEmployee/AddEmployee.jsx
+++ src/pages/Dahboard/AdminDashboard/pages/AddEmployee/AddEmployee.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./AddEmployee.css"
 
+
 const AddEmployee = () => {
+[userInfo2, setUserInfo2] = useState()
+const [name, setName] = useState('')
+const [email, setEmail] = useState('')
+const [department, setDepartment] = useState('')
+const [role, setRole] = useState('')
+
+const AddEmployeeInput = {
+  name:name,
+  email:email,
+  department:department,
+  role:role
+}
+
+async function handleAddEmployee() {
+  try {
+    setLoading(true);
+    const res = await axios.post(
+      "https://staftrack360.onrender.com/api/v1/addStaff/65d7af55ec77f213c8ff8978",
+      AddEmployeeInput
+    );
+    console.log(res)
+    
+    // localStorage.setItem('UserInfo2', JSON.stringify(res.data.data))
+  } catch (err) {
+    console.log("Error from api", err);
+   
+  }
+}
+
   return (
     <div className='AddEmployee'>
       <div className='sectionHead'>
@@ -9,11 +39,11 @@ const AddEmployee = () => {
       </div>
       
       <div className='Section1'>
-          <input type="text" placeholder='Name'/>
-          <input type="text" placeholder='Email'/>
-          <input type="text" placeholder='Department'/>
-          <input type="text" placeholder='Role'/>
-          <button>ADD EMPLOYEE</button>
+          <input type="text" placeholder='Name' onChange={(e)=>setName(e.target.value)}/>
+          <input type="email" placeholder='Email' onChange={(e)=>setEmail(e.target.value)}/>
+          <input type="text" placeholder='Department' onChange={(e)=>setDepartment(e.target.value)}/>
+          <input type="text" placeholder='Role' onChange={(e)=>setRole(e.target.value)}/>
+          <button onClick={handleAddEmployee}>ADD EMPLOYEE</button>
       </div>
     </div>
   )
