3ad63ae52328a7785dafa3d97661daf68e4581f3
Chukwuemerie <chukwuemerie@fincra.com>
2024-03-05 18:54:23 +0100

    optional on private route

--- src/pages/context/PrivateEmployee.jsx
+++ src/pages/context/PrivateEmployee.jsx
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from 'react-router-dom';
 const PrivatePageEmployee = () => {
   // const userInfo = JSON.parse(localStorage.getItem("loginUserInfo"));
   const userInfo2 = JSON.parse(localStorage.getItem("loginUserInfo2"));
-const userinfoMAin = userInfo2[0].token
+const userinfoMAin = userInfo2[0]?.token
 console.log(userinfoMAin)
   
   return (
